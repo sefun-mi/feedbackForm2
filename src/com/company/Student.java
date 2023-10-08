fd1517ae4f274598d509ec9c0fab36cabbaadf8f
sefunmi <iv.vi.kyle@gmail.com>
2023-10-08 23:24:31 +0100

    new class

---
+++ src/com/company/Student.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Student {
+    private String name;
+    private studentClass stdClass;
+
+    public enum studentClass{
+        SS1,
+        SS2,
+        SS3
+    }
+}
